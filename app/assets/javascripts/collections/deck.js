d1dd2bd5fea5b5a0c8c212c4e2722f2be3144ce3
Ted Whang <ted@quve.com>
2015-04-02 17:46:50 -0700

    Set values of cards

--- app/assets/javascripts/collections/deck.js
+++ app/assets/javascripts/collections/deck.js
@@ -2,7 +2,7 @@ Blackjack.Collections.Deck = Backbone.Collection.extend({
   model: Blackjack.Models.Card,
 
   initialize: function() {
-    var numbers = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+    var numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     var suits = ['S', 'C', 'D', 'H'];
 
     _.each(numbers, _.bind(function(number) {
--- app/assets/javascripts/models/card.js
+++ app/assets/javascripts/models/card.js
@@ -1,3 +1,16 @@
 Blackjack.Models.Card = Backbone.Model.extend({
 
+  initialize: function() {
+    this.setValue();
+  },
+
+  setValue: function() {
+    if (this.get('number') === 'A') {
+      this.set('value', 11);
+    } else if (_.contains(['10', 'J', 'Q', 'K'], this.get('number'))) {
+      this.set('value', 10);
+    } else {
+      this.set('value', parseInt(this.get('number'), 10));
+    }
+  }
 });
